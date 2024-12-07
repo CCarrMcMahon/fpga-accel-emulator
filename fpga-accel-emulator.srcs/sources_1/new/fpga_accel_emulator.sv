e2373237bf0ee2d5200a083ec6416d4be23224ff
CCarrMcMahon <CCarrMcMahon@gmail.com>
2024-12-07 01:02:27 -0600

    Add a docstring to fpga_accel_emulator.sv

--- fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
+++ fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
@@ -1,4 +1,23 @@
 `timescale 1ns / 1ps
+/**
+ * FPGA accelerator emulator.
+ *
+ * This module is a work in progress and currently receives data through a UART interface
+ * and displays the received data on LEDs. It uses a `uart_receiver` module to handle
+ * the UART communication.
+ *
+ * ## Inputs
+ * - `clk100mhz` (logic): 100 MHz input clock signal.
+ * - `cpu_resetn` (logic): Active-low reset signal.
+ * - `uart_txd_in` (logic): UART transmit data input.
+ *
+ * ## Outputs
+ * - `ja` (logic [0:0]): Output signal indicating data readiness.
+ * - `led` (logic [7:0]): Output LEDs displaying the received data.
+ *
+ * The module instantiates a `uart_receiver` to receive data at a baud rate of 9600.
+ * The received data is then output to the LEDs, and a signal indicates when data is ready.
+ */
 module fpga_accel_emulator (
     input logic clk100mhz,
     input logic cpu_resetn,
@@ -6,8 +25,11 @@ module fpga_accel_emulator (
     output logic [0:0] ja,
     output logic [7:0] led
 );
+    /* Signals */
     logic data_ready;
     logic [7:0] data_out;
+    assign ja  = data_ready;
+    assign led = data_out;
 
     // Instantiate the uart_receiver module
     uart_receiver #(
@@ -20,7 +42,4 @@ module fpga_accel_emulator (
         .data_ready(data_ready),
         .data_out(data_out)
     );
-
-    assign ja[0]  = data_ready;
-    assign led = data_out;
 endmodule
