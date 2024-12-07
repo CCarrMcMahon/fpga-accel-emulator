afd3bb098dc5cfa632f5c66c1afeefcacb4eb07a
CCarrMcMahon <CCarrMcMahon@gmail.com>
2024-12-07 00:16:49 -0600

    Remove debugging outputs

--- fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
+++ fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
@@ -3,15 +3,11 @@ module fpga_accel_emulator (
     input logic clk100mhz,
     input logic cpu_resetn,
     input logic uart_txd_in,
-    output logic [8:0] led,
-    output logic [3:0] ja,
-    output logic [7:0] jb
+    output logic [0:0] ja,
+    output logic [7:0] led
 );
-    logic [7:0] data_out;
     logic data_ready;
-
-    logic debug_baud_pulse_out;
-    logic [1:0] debug_uart_state;
+    logic [7:0] data_out;
 
     // Instantiate the uart_receiver module
     uart_receiver #(
@@ -21,13 +17,10 @@ module fpga_accel_emulator (
         .clk(clk100mhz),
         .reset(~cpu_resetn),
         .rx(uart_txd_in),
-        .data_out(data_out),
         .data_ready(data_ready),
-        .debug_baud_pulse_out(debug_baud_pulse_out),
-        .debug_uart_state(debug_uart_state)
+        .data_out(data_out)
     );
 
-    assign led = {data_ready, data_out};
-    assign ja  = {debug_uart_state, debug_baud_pulse_out, uart_txd_in};
-    assign jb  = data_out;
+    assign ja[0]  = data_ready;
+    assign led = data_out;
 endmodule
