42907c5b3d1d6cd1efbf64e8b56ab52d6abd545d
CCarrMcMahon <CCarrMcMahon@gmail.com>
2024-12-08 12:57:39 -0600

    Implement spi_master in top level module and output data on pins

--- fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
+++ fpga-accel-emulator.srcs/sources_1/new/fpga_accel_emulator.sv
@@ -20,14 +20,24 @@ module fpga_accel_emulator (
     input logic cpu_resetn,
     input logic uart_txd_in,
     input logic btnc,
-    output logic [2:0] ja,
+    output logic [7:0] ja,
+    output logic [1:0] jb,
     output logic [7:0] led
 );
-    // Internal Signals
-    logic [7:0] data_out;
-    logic data_ready;
-    logic data_error;
-    logic debug_baud_pulse_out;
+    // UART
+    logic [7:0] uart_data_out;
+    logic uart_data_ready;
+    logic uart_data_error;
+
+    // SPI
+    logic mosi;
+    logic miso = 0;
+    logic sclk;
+    logic csn;
+    logic [7:0] spi_data_out;
+    logic spi_data_ready;
+    logic spi_ack_data_read;
+    logic spi_data_error;
 
     // Instantiate the uart_receiver module
     uart_receiver #(
@@ -36,14 +46,35 @@ module fpga_accel_emulator (
     ) uart_receiver_inst (
         .clk(clk100mhz),
         .resetn(cpu_resetn),
+        .data_read(spi_ack_data_read),
         .rx(uart_txd_in),
-        .data_out(data_out),
-        .data_ready(data_ready),
+        .data_out(uart_data_out),
+        .data_ready(uart_data_ready),
+        .data_error(uart_data_error)
+    );
+
+    // Instantiate the spi_master module
+    spi_master #(
+        .ClkFreq (100_000_000),
+        .SclkFreq(1_000_000)
+    ) spi_master_inst (
+        .clk(clk100mhz),
+        .resetn(cpu_resetn),
+        .start_tx(uart_data_ready),
         .data_read(btnc),
-        .data_error(data_error)
+        .mosi(mosi),
+        .miso(miso),
+        .sclk(sclk),
+        .csn(csn),
+        .data_in(uart_data_out),
+        .data_out(spi_data_out),
+        .data_ready(spi_data_ready),
+        .ack_data_read(spi_ack_data_read),
+        .data_error(spi_data_error)
     );
 
     // Final Assignments
-    assign ja  = {data_error, data_ready, uart_txd_in};
-    assign led = data_out;
+    assign ja  = {spi_data_ready, csn, sclk, miso, mosi, spi_ack_data_read, uart_data_ready, uart_txd_in};
+    assign jb  = {spi_data_error, uart_data_error};
+    assign led = {spi_data_out};
 endmodule
