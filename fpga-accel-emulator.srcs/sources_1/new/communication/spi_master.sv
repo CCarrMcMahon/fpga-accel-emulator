ffdc948f25d94869f07104351f95be76b1cc16e8
CCarrMcMahon <CCarrMcMahon@gmail.com>
2024-12-10 23:58:05 -0600

    Rework spi_master

--- fpga-accel-emulator.srcs/sources_1/new/communication/spi_master.sv
+++ fpga-accel-emulator.srcs/sources_1/new/communication/spi_master.sv
@@ -2,215 +2,231 @@
  * @module spi_master
  * @brief SPI Master Module
  *
- * This module implements a SPI master that communicates with SPI slave devices. It transmits data from `data_in` to the
- * `mosi` output and receives data from the `miso` input, outputting the received byte on `data_out`. The module
- * operates at a configurable SPI clock frequency and system clock frequency.
+ * This module implements an SPI master that communicates with an SPI slave device. It supports a configurable clock
+ * frequency and data transfer rate.
  *
  * @param ClkFreq  The frequency of the input clock in Hz (default: 100 MHz).
- * @param SclkFreq The desired SPI serial clock frequency in Hz (default: 1 MHz).
+ * @param SclkFreq The desired SPI clock frequency in Hz (default: 1 MHz).
  *
  * @input clk          The system clock input.
  * @input resetn       Active-low reset signal.
- * @input start_tx     Signal to start the SPI transmission.
- * @input data_out_ack Acknowledgment signal indicating that the data has been read.
- * @input miso         SPI Master In Slave Out data input.
+ * @input start        Signal to start the SPI transfer.
  * @input data_in      The byte of data to be transmitted.
+ * @input miso         Master In Slave Out data signal.
+ * @input data_out_ack Acknowledgment signal indicating that the data has been read.
  *
- * @output mosi           SPI Master Out Slave In data output.
- * @output sclk           SPI serial clock output.
- * @output csn            Chip select (active low) output.
- * @output data_out       The received byte of data.
- * @output data_out_ready Indicates that a new byte of data is available.
- * @output data_in_stored Indicates that the input data has been stored for transmission.
- * @output data_error     Indicates an error in the data transmission or reception.
+ * @output data_out    The received byte of data.
+ * @output mosi        Master Out Slave In data signal.
+ * @output sclk        SPI clock signal.
+ * @output csn         Chip select signal (active low).
+ * @output data_in_ack Acknowledgment signal indicating that the data has been stored.
+ * @output valid       Indicates that a new byte of data is available.
+ * @output error       Indicates an error in the SPI transfer.
  *
- * The module uses a state machine to manage the transmission process, which includes the following states:
- * - IDLE: Waiting for the start signal.
- * - START: Preparing for data transmission.
- * - DATA: Transmitting and receiving data bits.
- * - STOP: Finalizing the transmission and outputting the received byte.
+ * The module uses a state machine to manage the SPI communication process. The states include:
+ * - RESET: Initializes the internal signals and prepares the module for operation.
+ * - IDLE: Waits for a start signal to begin the SPI transfer.
+ * - START: Prepares for data transfer by asserting the chip select signal and initializing the shift register.
+ * - TRANSFER: Handles the actual data transfer, shifting data into and out of the shift register with the SPI clock.
+ * - DONE: Indicates that the data transfer is complete and prepares to output the received data.
+ * - TRANSFER_OUT_ACKED: Clears the output data and status signals after the data has been acknowledged.
+ * - ERROR: Handles any errors that occur during the SPI transfer.
  *
- * The module also includes internal logic for synchronizing the `start_tx` and `data_out_ack` signals, and a clock
+ * The module also includes internal logic for synchronizing the `start` and `data_out_ack` signals, sand a clock
  * generator for generating the SPI clock.
  */
 module spi_master #(
-    parameter int ClkFreq = 100_000_000,  // Input clock frequency in Hz
-    parameter int SclkFreq = 1_000_000  // SPI serial clock frequency in Hz
+    parameter int ClkFreq  = 100_000_000,
+    parameter int SclkFreq = 1_000_000
 ) (
-    // Clock and Reset
+    // Control Signals
     input logic clk,
     input logic resetn,
+    input logic start,
 
-    // Control Signals
-    input logic start_tx,
+    // Data Input Signals
+    input logic [7:0] data_in,
     input logic data_out_ack,
 
-    // SPI Interface
-    output logic mosi,
+    // SPI Interface Signals
     input  logic miso,
+    output logic mosi,
     output logic sclk,
     output logic csn,
 
-    // Data Signals
-    input  logic [7:0] data_in,
+    // Data Output Signals
     output logic [7:0] data_out,
+    output logic data_in_ack,
 
     // Status Signals
-    output logic data_out_ready,
-    output logic data_in_stored,
-    output logic data_error
+    output logic valid,
+    output logic error
 );
     // States
-    typedef enum logic [1:0] {
+    typedef enum logic [2:0] {
+        RESET,
         IDLE,
         START,
-        DATA,
-        STOP
+        TRANSFER,
+        DONE,
+        TRANSFER_OUT_ACKED,
+        ERROR
     } state_t;
-    state_t current_state, next_state;
+    state_t state, next_state;
 
     // Internal Signals
-    logic clear_sclk_clock_gen;
-    logic [3:0] bit_counter;
+    logic clear_sclk_gen;
+    logic start_synced;
+    logic data_out_ack_synced;
+    logic [3:0] data_counter;
     logic [7:0] shift_reg;
     logic prev_sclk;
-    logic synced_start_tx;
-    logic synced_data_out_ack;
 
     // Instantiate a clock generator for the SPI clock
     clock_generator #(
         .ClkInFreq (ClkFreq),
         .ClkOutFreq(SclkFreq)
-    ) sclk_freq_clock_generator_inst_1 (
+    ) sclk_gen (
         .clk_in (clk),
         .resetn (resetn),
-        .clear  (clear_sclk_clock_gen),
+        .clear  (clear_sclk_gen),
         .clk_out(sclk)
     );
 
-    // Instantiate a synchronizer for start_tx
-    synchronizer start_tx_synchronizer_inst_1 (
+    // Instantiate a synchronizer for the start signal
+    synchronizer start_sync (
         .clk(clk),
         .resetn(resetn),
-        .async_signal(start_tx),
-        .sync_signal(synced_start_tx)
+        .async_signal(start),
+        .sync_signal(start_synced)
     );
 
     // Instantiate a synchronizer for data_out_ack
-    synchronizer data_out_ack_synchronizer_inst_1 (
+    synchronizer data_out_ack_sync (
         .clk(clk),
         .resetn(resetn),
         .async_signal(data_out_ack),
-        .sync_signal(synced_data_out_ack)
+        .sync_signal(data_out_ack_synced)
     );
 
     // State Machine Transitions
     always_ff @(posedge clk or negedge resetn) begin
         if (!resetn) begin
-            current_state <= IDLE;
-        end else if (!synced_data_out_ack) begin  // Only update state when data_out is not being read
-            current_state <= next_state;
+            state <= RESET;
+        end else begin
+            state <= next_state;
         end
     end
 
     // State Machine Logic
     always_comb begin
-        next_state = current_state;
-        case (current_state)
+        next_state = state;
+        case (state)
+            RESET: begin
+                next_state = IDLE;
+            end
             IDLE: begin
-                // Start transmission requested and previous data has been read
-                if (synced_start_tx && !data_out_ready) begin
-                    next_state = START;
+                if (start_synced) begin
+                    // Previous data has been processed
+                    if (!valid) begin
+                        next_state = START;
+                    end else begin
+                        next_state = ERROR;
+                    end
+                end else if (data_out_ack_synced) begin  // Give processing priority
+                    next_state = TRANSFER_OUT_ACKED;
                 end
             end
             START: begin
-                if (data_in_stored) begin
-                    next_state = DATA;
+                next_state = TRANSFER;
+            end
+            TRANSFER: begin
+                if (data_counter == 8) begin
+                    next_state = DONE;
                 end
             end
-            DATA: begin
-                // Wait for a clock transition from high to low after all bits have been transferred
-                if (prev_sclk == !sclk && bit_counter == 8) begin
-                    next_state = STOP;
-                end
+            DONE: begin
+                next_state = IDLE;
             end
-            STOP: begin
-                if (data_out_ready) begin
-                    next_state = IDLE;
-                end
+            TRANSFER_OUT_ACKED: begin
+                next_state = IDLE;
             end
-            default: next_state = IDLE;
+            ERROR: begin
+                next_state = IDLE;
+            end
+            default: next_state = ERROR;
         endcase
     end
 
     // SPI Master Logic
-    always_ff @(posedge clk or negedge resetn) begin
-        if (!resetn) begin
-            clear_sclk_clock_gen <= 1;
-            bit_counter <= 0;
-            shift_reg <= 0;
-            prev_sclk <= 1;
-            mosi <= 0;
-            csn <= 1;
-            data_out <= 0;
-            data_out_ready <= 0;
-            data_in_stored <= 0;
-            data_error <= 0;
-        end else begin
-            if (synced_data_out_ack) begin
+    always_ff @(posedge clk) begin
+        case (state)
+            RESET: begin
+                mosi <= 0;
+                csn <= 1;
                 data_out <= 0;
-                data_out_ready <= 0;
-                data_error <= 0;
+                data_in_ack <= 0;
+                valid <= 0;
+                error <= 0;
+                clear_sclk_gen <= 1;
+                data_counter <= 0;
+                shift_reg <= 0;
+                prev_sclk <= 0;
             end
+            IDLE: begin
+                mosi <= 0;
+                csn <= 1;
+                data_in_ack <= 0;
+                clear_sclk_gen <= 1;
+                data_counter <= 0;
+                shift_reg <= 0;
+                prev_sclk <= 0;
+            end
+            START: begin
+                // Enable clock and chip
+                csn <= 0;
+                clear_sclk_gen <= 0;
 
-            case (current_state)
-                IDLE: begin
-                    // Keep the clock and chip disabled while idle
-                    clear_sclk_clock_gen <= 1;
-                    prev_sclk <= 1;
-                    mosi <= 0;
-                    csn <= 1;
-                    if (synced_start_tx && data_out_ready) begin
-                        // New data detected but previous data hasn't been read
-                        data_error <= 1;
+                // Set inverse so first bit is sent immediately
+                prev_sclk <= ~sclk;
+
+                // Store input data and ack that is has been read
+                shift_reg <= data_in;
+                data_in_ack <= 1;
+            end
+            TRANSFER: begin
+                // Clear signal now that it has been read
+                data_in_ack <= 0;
+
+                // Wait for clock transition (first bit should be sent immediately)
+                if (sclk != prev_sclk) begin
+                    prev_sclk <= sclk;
+
+                    if (sclk) begin
+                        // Shift miso into LSB when clk transitions from low to high
+                        shift_reg <= {shift_reg[6:0], miso};
+                        data_counter <= data_counter + 1;
+                    end else begin
+                        // Output MSB of mosi when clk transitions from high to low
+                        mosi <= shift_reg[7];
                     end
                 end
-                START: begin
-                    // Enable clock and chip
-                    clear_sclk_clock_gen <= 0;
-                    prev_sclk <= ~sclk;  // Set inverse so first bit is sent immediately
-                    bit_counter <= 0;
-                    csn <= 0;
-
-                    // Store input data and ack that is has been read
-                    shift_reg <= data_in;
-                    data_in_stored <= 1;
-                end
-                DATA: begin
-                    // Clear signal now that it has been read
-                    data_in_stored <= 0;
-
-                    // Wait for clock transition (first bit should be sent immediately)
-                    if (sclk != prev_sclk && bit_counter < 8) begin
-                        prev_sclk <= sclk;
-
-                        if (sclk) begin
-                            // Shift miso into LSB when clk transitions from low to high
-                            shift_reg   <= {shift_reg[6:0], miso};
-                            bit_counter <= bit_counter + 1;
-                        end else begin
-                            // Output MSB of mosi when clk transitions from high to low
-                            mosi <= shift_reg[7];
-                        end
-                    end
-                end
-                STOP: begin
-                    // Output received data and indicate it is ready
-                    data_out <= shift_reg;
-                    data_out_ready <= 1;
-                end
-                default: data_error <= 1;
-            endcase
-        end
+            end
+            DONE: begin
+                // Output received data and indicate it is ready
+                data_out <= shift_reg;
+                valid <= 1;
+            end
+            TRANSFER_OUT_ACKED: begin
+                // Clear data_out and reset status signals
+                data_out <= 0;
+                valid <= 0;
+                error <= 0;
+            end
+            ERROR: begin
+                error <= 1;
+            end
+            default: error <= 1;
+        endcase
     end
 endmodule
